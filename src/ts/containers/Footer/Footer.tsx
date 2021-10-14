169a5cc90e57eed6efb258cb97d28efa6efe91ae
Troy DeSpain <despaintroy@gmail.com>
2021-10-14 10:42:17 -0600

    Update subheader font

--- src/ts/containers/Footer/Footer.tsx
+++ src/ts/containers/Footer/Footer.tsx
@@ -5,9 +5,7 @@ import { Container } from 'react-bootstrap'
 function Footer(): React.ReactElement {
 	return (
 		<div className='footer py-5'>
-			<Container>
-				
-			</Container>
+			<Container></Container>
 		</div>
 	)
 }
