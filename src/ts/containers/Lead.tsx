51286bdac1e9fb6e87ae16691e13cbeef41d7963
Troy DeSpain <despaintroy@gmail.com>
2021-10-17 23:43:33 -0600

    Fix vanta background on production

--- src/ts/containers/Lead.tsx
+++ src/ts/containers/Lead.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { Button, Container } from 'react-bootstrap'
+import * as THREE from 'three'
+import WAVES from 'vanta/dist/vanta.waves.min'
 
 function Lead(): React.ReactElement {
+	const [vantaEffect, setVantaEffect] = useState(0)
+	const vantaBackground = useRef(null)
+	useEffect(() => {
+		if (!vantaEffect) {
+			setVantaEffect(
+				WAVES({
+					el: vantaBackground.current,
+					THREE,
+					mouseControls: false,
+					touchControls: false,
+					gyroControls: false,
+					minHeight: 200.0,
+					minWidth: 200.0,
+					color: '#0f2439',
+					shininess: 20,
+					waveHeight: 10,
+					waveSpeed: 1.05,
+					zoom: 1.1,
+				})
+			)
+		}
+		return (): void => {
+			if (vantaEffect) (vantaEffect as unknown as any).destroy()
+		}
+	}, [vantaEffect])
+
 	return (
-		<div id='vanta-bg' className='bg-primary full-height lead'>
+		<div
+			id='vanta-bg'
+			className='bg-primary full-height lead'
+			ref={vantaBackground}
+		>
 			<Container>
 				<div>
 					<h1>Hack USU</h1>
