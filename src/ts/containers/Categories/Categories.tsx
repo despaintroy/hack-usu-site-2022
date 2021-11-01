4981b4e5427a106d0a6fd66d89e5fcc8ee2e4ffd
Troy DeSpain <despaintroy@gmail.com>
2021-11-01 09:30:06 -0600

    Display project ideas for each category

--- src/ts/containers/Categories/Categories.tsx
+++ src/ts/containers/Categories/Categories.tsx
@@ -10,12 +10,20 @@ function Categories(): React.ReactElement {
 		<div className='category-tile' key={category.title}>
 			<div className='title'>{category.title}</div>
 			<div className='description'>{category.description}</div>
+			{category.ideas.length > 0 && (
+				<details className='ideas'>
+					<summary>Project Ideas</summary>
+					<ul>
+						{category.ideas.map((suggestion: string, index: number) => (<li key={index}>{suggestion}</li>))}
+					</ul>
+				</details>
+			)}
 		</div>
 	)
 
 	return (
 		<div className='categories py-5' id='prizes'>
-			<Container className='narrow'>
+			<Container>
 				<Subheader text='Prze Categories' />
 				<p className='fs-5'>
 					You can start thinking of project ideas before the event, or
--- src/ts/containers/Categories/categoriesContent.ts
+++ src/ts/containers/Categories/categoriesContent.ts
@@ -1,31 +1,42 @@
 export interface CategoriesData {
 	title: string
 	description: string
+	ideas: string[]
 }
 
 export const categoriesList: CategoriesData[] = [
 	{
 		title: 'General',
 		description: 'Projects that don\'t fit the other categories!',
+		ideas: []
 	},
 	{
 		title: 'Data Analytics & Visualization',
 		description: 'Demonstrate ways of gaining insights or displaying data in a creative way.',
+		ideas: []
 	},
 	{
 		title: 'Game Development',
 		description: 'Any game. Any technology.',
+		ideas: []
 	},
 	{
 		title: 'Privacy & Security',
 		description: 'Show us your great ideas related to user privacy, improved authentication methods, or system security.',
+		ideas: [
+			'Design a more secure user authentication system',
+			'Create a tool to help users test their passwords\' strength',
+			'Write a program to test people\'s ability to avoid phishing emails',
+		]
 	},
 	{
 		title: 'Hardware',
 		description: 'All projects focused on physical hardware.',
+		ideas: []
 	},
 	{
 		title: 'AI & Machine Learning',
 		description: 'Show us how your program learns how to solve a problem!',
+		ideas: []
 	},
 ]
