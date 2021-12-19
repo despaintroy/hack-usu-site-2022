a1a239cf6ec4bffe75a82983ced364ef0ce37b45
Troy DeSpain <despaintroy@gmail.com>
2021-12-18 19:57:51 -0700

    Update server invite link

--- src/ts/utils/constants.ts
+++ src/ts/utils/constants.ts
@@ -6,5 +6,5 @@ export const REGISTRATION_LINK = 'https://forms.gle/uGArvUcgqVm8Keh29'
 export const START_TIME = new Date('03/18/2022 4:00 PM')
 
 // CONTACT
-export const DISCORD_LINK = 'https://discord.gg/mDUKePRGx7'
+export const DISCORD_LINK = 'https://discord.gg/DpjrzqBwq5'
 export const CONTACT_EMAIL = 'hackusu@aggies.usu.edu'
