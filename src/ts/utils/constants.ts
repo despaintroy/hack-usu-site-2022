4ab204eb77943f5a465b5075ce74274fca05fc0b
Troy DeSpain <despaintroy@gmail.com>
2022-01-02 16:17:03 -0700

    Update registration link

--- src/ts/utils/constants.ts
+++ src/ts/utils/constants.ts
@@ -2,7 +2,8 @@
 export const REGISTRATION_OPEN = false
 export const REGISTRATION_CLOSED_MESSAGE =
 	'Registration opens late January 2022'
-export const REGISTRATION_LINK = 'https://forms.gle/uGArvUcgqVm8Keh29'
+export const REGISTRATION_LINK =
+	'https://hackusu2022.eventbrite.com/?aff=hackusu' // 'https://forms.gle/uGArvUcgqVm8Keh29'
 export const START_TIME = new Date('03/18/2022 4:00 PM')
 
 // CONTACT
