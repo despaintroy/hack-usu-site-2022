fd1f9a33b99e3fb6dbe016bc8b1a1f102c259365
Troy DeSpain <despaintroy@gmail.com>
2022-01-20 23:55:00 -0700

    registration banner

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,33 +1,46 @@
 import React, { FC } from 'react'
 
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { REGISTRATION_LINK, REGISTRATION_OPEN } from 'ts/utils/constants'
 
 const Navigation: FC = () => {
 	return (
-		<Navbar
-			bg='primary'
-			variant='dark'
-			className='navigation'
-			fixed='top'
-			collapseOnSelect
-			expand='md'
-		>
-			<Container>
-				<Navbar.Brand href='#top'>HackUSU 2022</Navbar.Brand>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav>
-						<Nav.Link href='#registration'>Register</Nav.Link>
-						<Nav.Link href='#about'>About</Nav.Link>
-						<Nav.Link href='#categories'>Categories</Nav.Link>
-						<Nav.Link href='#location'>Location</Nav.Link>
-						<Nav.Link href='#faq'>FAQ</Nav.Link>
-						<Nav.Link href='#schedule'>Schedule</Nav.Link>
-						<Nav.Link href='#contact'>Contact</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+		<>
+			<Navbar
+				bg='primary'
+				variant='dark'
+				className='navigation'
+				fixed='top'
+				collapseOnSelect
+				expand='md'
+			>
+				<Container>
+					<Navbar.Brand href='#top'>HackUSU 2022</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav>
+							<Nav.Link href='#registration'>Register</Nav.Link>
+							<Nav.Link href='#about'>About</Nav.Link>
+							<Nav.Link href='#categories'>Categories</Nav.Link>
+							<Nav.Link href='#location'>Location</Nav.Link>
+							<Nav.Link href='#faq'>FAQ</Nav.Link>
+							<Nav.Link href='#schedule'>Schedule</Nav.Link>
+							<Nav.Link href='#contact'>Contact</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			{REGISTRATION_OPEN && (
+				<div
+					className='registration-banner'
+					onClick={(): void => {
+						window.open(REGISTRATION_LINK, '_blank')
+					}}
+				>
+					<Container>Register for Free</Container>
+				</div>
+			)}
+		</>
 	)
 }
 
