d4bc88c71b53320a5157c3e95fd0812a8cae97a5
Troy DeSpain <despaintroy@gmail.com>
2022-02-13 01:17:52 -0700

    Add sdl to sponsors

--- src/ts/containers/Sponsors/sponsorData.ts
+++ src/ts/containers/Sponsors/sponsorData.ts
@@ -6,9 +6,24 @@ interface SponsorData {
 
 export const sponsorData: SponsorData[] = [
 	{
+		name: 'USU Huntsman School of Engineering',
+		fileName: 'huntsman-school.jpg',
+		url: 'https://huntsman.usu.edu/',
+	},
+	{
+		name: 'USU College of Science',
+		fileName: 'college-of-science.png',
+		url: 'https://cs.usu.edu/',
+	},
+	{
 		name: 'Lightning Kite',
 		fileName: 'lightning-kite.svg',
 		url: 'https://lightningkite.com/',
+	},
+	{
+		name: 'Space Dynamics Laboratory',
+		fileName: 'sdl.png',
+		url: 'https://sdl.usu.edu/',
 	},
 	{
 		name: 'XMission',
@@ -19,15 +34,5 @@ export const sponsorData: SponsorData[] = [
 		name: 'VLCM',
 		fileName: 'vlcm.png',
 		url: 'https://www.vlcmtech.com/',
-	},
-	{
-		name: 'USU College of Science',
-		fileName: 'college-of-science.png',
-		url: 'https://cs.usu.edu/',
-	},
-	{
-		name: 'USU Huntsman School of Engineering',
-		fileName: 'huntsman-school.jpg',
-		url: 'https://huntsman.usu.edu/',
 	},
 ]
